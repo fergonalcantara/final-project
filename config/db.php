41f26034be682ffbabec28e9c231c8dba122a157
fergonalcantara <fergonalcantara@gmail.com>
2025-09-29 00:04:20 -0600

    Structure and db connection

---
+++ config/db.php
@@ -0,0 +1,20 @@
+<?php
+$env = parse_ini_file(__DIR__ . '/../.env');
+
+$host = $env['DB_HOST'];
+$port = $env['DB_PORT'];
+$db   = $env['DB_NAME'];
+$user = $env['DB_USER'];
+$pass = $env['DB_PASS'];
+
+try {
+    $conn = new PDO(
+        "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4",
+        $user,
+        $pass
+    );
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die(" Error de conexión: " . $e->getMessage());
+}
+?>
